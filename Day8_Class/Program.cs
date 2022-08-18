b3ec1b9d3f2f644c0a0c8997a0c325d8d1c370e2
Garden <66628342+ljw9611@users.noreply.github.com>
2022-08-19 01:42:43 +0900

    Update

---
+++ Day8_Class/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class MyFirstClass
+{
+    static void StaticMethod() => Console.WriteLine("[1] 정적 메서드");
+
+    void InstanceMethod() => Console.WriteLine("[2] 인스턴스 메서드");
+    static void Main()
+    {
+        MyFirstClass.StaticMethod();            // 1. 정적 메서드 호출
+        MyFirstClass my = new MyFirstClass();   // 2. 인스턴스 메서드 호출
+        my.InstanceMethod();
+    }
+}
