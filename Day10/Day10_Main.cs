4f06964d52a297ee90e0167b7cb85f2ff648cbfa
Garden <66628342+ljw9611@users.noreply.github.com>
2022-09-15 23:05:33 +0900

    Day10 GenericStack 업데이트

---
+++ Day10/Day10_Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using N_GenericStack;
+namespace Day10
+{
+    internal class Day10_Main
+    {
+        static void Main(string[] args)
+        {
+            GenericStack.StackEx();
+        }
+    }
+}
---
+++ Day10/GenericStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace N_GenericStack
+{
+    internal class GenericStack
+    {
+        static public void StackEx()
+        {
+            //제네릭 클래스가 아닌 Stack 클래스
+            Stack stack = new Stack(); //Collections
+            stack.Push(1234);
+            int num = (int)stack.Pop();
+            Console.WriteLine(num);
+
+            //제네릭 클래스 사용
+            Stack<int> stack2 = new Stack<int>(); //Collections.Generic
+            stack2.Push(12345);
+            int num2 = stack2.Pop();
+            Console.WriteLine(num2);
+        }
+    }
+}
