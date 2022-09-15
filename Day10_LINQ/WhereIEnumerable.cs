4b8df5d937b6b5907f896b43e91bbc1ab1b6e4f4
Garden <66628342+ljw9611@users.noreply.github.com>
2022-09-16 05:55:02 +0900

    Day10 LINQ - Where / IEnumerable 예제 추가

--- Day10_LINQ/WhereIEnumerable.cs
+++ Day10_LINQ/WhereIEnumerable.cs
@@ -14,6 +14,13 @@ namespace N_WhereIEnumerable
             {
                 Console.WriteLine(check);
             }
+
+            //ToList() 메서드로 IEnumerable<T>를 List<T>로 변환하기
+            List<int> list = numbers.Where(num => num == 3).ToList();
+            foreach (var check in list)
+            {
+                Console.WriteLine(check);
+            }
         }
     }
 }
