98586d48d759daba736cd1aac74d61e2da548e00
Garden <66628342+ljw9611@users.noreply.github.com>
2022-10-02 23:57:50 +0900

    merge Algorithm update

---
+++ Day11_Algorithm/MergeAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace N_MergeAlgorithm
+{
+    internal class MergeAlgorithmClass
+    {
+        static public void MergeAlgorithm()
+        {
+            // 1. 입력
+            int[] first = { 1, 3, 5 }; //오름차순 정렬
+            int[] second = { 2, 4 }; //오름차순 정렬
+            int M = first.Length; int N = second.Length; //M:N 관행
+            int[] merge = new int[M + N];
+            int i = 0; int j = 0; int k = 0;
+
+            // 2. 처리
+            while (i < M && j < N)
+            {
+                if (first[i] <= second[j])
+                {
+                    merge[k++] = first[i++];
+                }
+                else
+                {
+                    merge[k++] = second[j++];
+                }
+                while(i <  M) // 첫 번째 배열이 끝에 도달할 때까지
+                {
+                    merge[k++] = first
+                }
+            }
+        }
+    }
+}
