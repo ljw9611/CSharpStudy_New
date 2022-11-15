be87032a5d567b5886b24d68c963c9dff3266505
Garden <66628342+ljw9611@users.noreply.github.com>
2022-11-15 19:49:55 +0900

    Update

---
+++ Day12_Object/InstanceEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Day12_Object
+{
+    public class InstanceEx 
+    {
+        // 인스턴스 메서드 예시
+        // 01. 인스턴스 메서드 생성
+        public void Sum(int x, int y)
+        {
+            int sum = x + y;
+            Console.WriteLine($"합계 : {sum}");
+            // 02. MyMath 클래스의 인스턴스 생성
+            InstanceEx myMath = new InstanceEx();
+            // 03. 개체.인스턴스메서드이름; 형태로 호출
+            myMath.Sum(3, 5);
+        }
+    }
+}
