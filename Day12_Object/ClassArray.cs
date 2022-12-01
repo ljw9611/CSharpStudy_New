11da3004f13c599241773b0624bb143b05cd31f6
Garden <66628342+ljw9611@users.noreply.github.com>
2022-12-01 11:58:52 +0900

    Day 12 - Object

--- Day12_Object/ClassArray.cs
+++ Day12_Object/ClassArray.cs
@@ -3,7 +3,24 @@ using System.Linq;
 
 namespace N_ClassArray
 {
-    internal class ClassArray
+    public class CategoryClass
     {
+        public void Print(int i) => Console.WriteLine($"카테고리 {i}");
+    }
+
+    public class ClassArray
+    {
+        public static void ClassArrayMain()
+        {
+            CategoryClass[] categories = new CategoryClass[3]; // 클래스 배열 생성
+            categories[0] = new CategoryClass();
+            categories[1] = new CategoryClass();
+            categories[2] = new CategoryClass();
+
+            for (int i = 0; i < categories.Length; i++)
+            {
+                categories[i].Print(i);
+            }
+        }
     }
 }
--- Day12_Object/Main.cs
+++ Day12_Object/Main.cs
@@ -2,6 +2,7 @@
 using N_ObjectNote;
 using N_InstanceEx;
 using N_ToStringMethod;
+using N_ClassArray;
 
 namespace main
 {
@@ -15,8 +16,10 @@ namespace main
             //InstanceEx myMath = new InstanceEx(); // 02. MyMath 클래스의 인스턴스 생성
             //myMath.Sum(3, 5); // 03. 개체.인스턴스메서드이름; 형태로 호출
 
-            ToStringMethodClass toStringMethodClass = new ToStringMethodClass();
-            toStringMethodClass.ToStringMethod();
+            //ToStringMethodClass toStringMethodClass = new ToStringMethodClass();
+            //toStringMethodClass.ToStringMethod();
+
+            ClassArray.ClassArrayMain(); // ClassArray.cs 실행
         }
     }
 }
