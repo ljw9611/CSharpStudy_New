9affa135f14e1f6b3d6d749de6bdb731fc6561e0
Garden <66628342+ljw9611@users.noreply.github.com>
2022-12-02 03:31:04 +0900

    Day13 - Namespace Update

---
+++ Day13/Day13_Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using N_NamespaceNote;
+using N_TypeDefinitionWithUsing;
+
+class Day13_Main
+{
+    static void Main(string[] args)
+    {
+        //NamespaceNote.Namespacenote();
+        TypeDefinitionWithUsing.TypeDefinitionWithusing();
+    }
+}
---
+++ Day13/Namespace/TypeDefinitionWithUsing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+ 
+
+namespace N_TypeDefinitionWithUsing
+{
+    using Project = Gitbut.Education.CSharp.Lecture;
+
+    public class TypeDefinitionWithUsing
+    {
+        public static void TypeDefinitionWithusing() 
+        {
+            Gitbut.Education.CSharp.Lecture lec = // 1. 기본 호출
+            new Gitbut.Education.CSharp.Lecture();
+            Console.WriteLine(lec);
+
+            Project p = new Project(); // 2. using 지시문 사용 호출
+            Console.WriteLine(p);
+        }
+    }
+}
+
+namespace Gitbut
+{
+    namespace Education
+    {
+        namespace CSharp
+        {
+            public class Lecture
+            {
+                public override string ToString()
+                {
+                    return "Lecture 클래스 호출됨";
+                }
+            }
+        }
+    }
+}
